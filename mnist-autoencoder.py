3ec873b29a1666e5e7fe625859df1ced706c2a12
HenriARM <henriarm@yandex.ru>
2021-01-25 14:28:29 +0200

    Added comments to mnist-autoencoder.py

--- mnist-autoencoder.py
+++ mnist-autoencoder.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 # from torch.autograd import Variable
 from torch.optim import Adam
+import torch.nn.functional
 
 
 class AE(nn.Module):
@@ -10,20 +11,20 @@ class AE(nn.Module):
         super(AE, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5),
             nn.ReLU(True),
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
             nn.ReLU(True))
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(in_channels=16, out_channels=6, kernel_size=5),
             nn.ReLU(True),
-            nn.ConvTranspose2d(out_channels=6, in_channels=3, kernel_size=5),
+            nn.ConvTranspose2d(out_channels=1, in_channels=6, kernel_size=5),
             nn.ReLU(True))
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        z = self.encoder(x)
+        x = self.decoder(z)
+        return x, z
 
 
 # # 32 x 32 - cifar
@@ -36,7 +37,7 @@ def main():
     # train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-    train_data = torchvision.datasets.MNIST(root='mnist-dataset/', train=True, download=True, transform=transform)
+    train_data = torchvision.datasets.MNIST(root='datasets/', train=True, download=True, transform=transform)
 
     BATCH_SIZE = 256
     EPOCHS = 2
@@ -47,7 +48,7 @@ def main():
 
     # #  use gpu if available
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # # load it to the specified device, either gpu or cpu
+    # # load it to the specified devicema, either gpu or cpu
     # model = AE(input_shape=784).to(device)
 
     model = AE().cpu()
@@ -55,22 +56,27 @@ def main():
     optimizer = Adam(model.parameters(), lr=lr, weight_decay=1e-5)
 
     for epoch in range(EPOCHS):
-        for image, _ in train_loader:
-            # label is not used  in AE
-            # image = Variable(image).cpu()
+        for x, _ in train_loader:
+            # TODO: add noise to x
+            x_noisy = x
 
-            # image = image.view(-1, 784) #.to(device)
+            # 1. AE = Dumb
+            # 2. DAE = Denoising AutoEncoder
+            # 3. β-VAE = Variational AutoEncoder
+            # 4. CCβ-VAE = https://arxiv.org/pdf/1804.00104.pdf
 
-            output = model.forward(image)
-            # TODO: convert output image too
-            loss = criterion.forward(output, image)
+            y_prim, z = model.forward(x_noisy)
+            loss = criterion.forward(y_prim, x)
+
+            # anomaly detection:
+            # ref_dist = 1.0 - torch.nn.functional.cosine_similarity(z_ref, z) # 0..2
 
             # reset gradients
             optimizer.zero_grad()
 
-            criterion.backward()
+            loss.backward()
             optimizer.step()
-        print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, EPOCHS, criterion.data()))
+        print('epoch [{}/{}], loss:{:.4f}'.format(epoch + 1, EPOCHS, criterion.data()))
 
 
 if __name__ == '__main__':
