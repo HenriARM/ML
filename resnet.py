2faf3af47d4b44f4cf5a526252c89e52118c0b53
HenriARM <henriarm@yandex.ru>
2021-01-25 14:21:03 +0200

    Updated ResNet with unfinished comments

--- resnet.py
+++ resnet.py
@@ -9,6 +9,8 @@ import torch
 from torch.optim import Adam
 from torchnet.meter import AverageValueMeter
 
+import torchvision.models.resnet
+
 DEVICE = 'cpu'
 if torch.cuda.is_available():
     DEVICE = 'cuda'
@@ -29,23 +31,28 @@ class ResidualBlock(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         # ResNet conv layers try not to change image size p = (k - 1)/2, our kernel is odd
+        # TODO: create conv_3x3 class
         self.layers = nn.Sequential(
-            conv_plus_norm(in_channels=self.in_channels, out_channels=self.out_channels, kernel_size=3, padding=1, stride=1),
+            conv_plus_norm(in_channels=self.in_channels, out_channels=self.out_channels, kernel_size=3, padding=1,
+                           stride=1),
             nn.ReLU(),
-            conv_plus_norm(in_channels=self.out_channels, out_channels=self.out_channels, kernel_size=3, padding=1, stride=1)
+            conv_plus_norm(in_channels=self.out_channels, out_channels=self.out_channels, kernel_size=3, padding=1,
+                           stride=1)
         )
-        # TODO: there is 2 ways to match spatial dimensions, conv 1x1 /2 or linear projection with data padding, no?
-        # TODO: how conv 1x1 with stride 2 will match if downsamples by half?
+        # TODO: change linear projection to conv with no W,H change
+        # TODO: dont use shortcut variable, do shortcut always in forward, since we store some vars in classs to see gradients
+
         self.shortcut = nn.Identity()
         # if self.in_channels != self.out_channels:
         #     self.shortcut = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=2, stride=2)
 
     def forward(self, x):
-        residual = self.shortcut(x)
+        residual = x
         x = self.layers(x)
         if self.in_channels != self.out_channels:
             # all filters in resnet are even
             shift = (self.out_channels - self.in_channels) // 2
+            # TODO: try also instead of conv use pooling layer of channels or upscaling
             residual = pad(residual, (0, 0, 0, 0, shift, shift, 0, 0), 'constant', 0)
         return x + residual
 
@@ -59,8 +66,7 @@ class ResidualGate(nn.Module):
         for i in range(blocks - 1):
             self.blocks.append(ResidualBlock(in_channels=out_channels, out_channels=out_channels))
 
-        # TODO: what is better put layers sequential inside sequential, or use list of all layers
-        #  and in forward() just loop them?
+        # TODO: use Sequential
         # self.blocks = nn.Sequential(
         #     ResidualBlock(in_channels=in_channels, out_channels=out_channels),
         #     *[ResidualBlock(in_channels=(out_channels), out_channels=out_channels for _ in range(blocks - 1)]
@@ -75,14 +81,9 @@ class ResidualGate(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, in_channels):
         super(Encoder, self).__init__()
-
-        # TODO: read about and use nn.ModuleList
-        # TODO: still didn't get when use striding for downsampling is better than pooling?
-        #  https://stats.stackexchange.com/questions/387482/pooling-vs-stride-for-downsampling/387522
         self.gate1 = nn.Sequential(
-            # TODO: should be bias false, so computations get faster? bias=False
             # out = (28 + 2*2 - 3) / 1 + 1   (28)
-            nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=3, stride=2, padding=0, bias=False),
             # TODO: read about BatchNorm2d
             nn.BatchNorm2d(16),
             nn.ReLU(),
@@ -101,14 +102,15 @@ class Encoder(nn.Module):
         return x
 
 
+# TODO: don't use Decoder architecture, put all Encoder and Decoder in ResNet class
 class Decoder(nn.Module):
     def __init__(self, in_features, n_classes):
         super(Decoder, self).__init__()
-        self.avg = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        # self.avg = nn.AdaptiveAvgPool2d(output_size=(1, 1))
         self.fc = nn.Linear(in_features=in_features, out_features=n_classes)
 
     def forward(self, x):
-        x = self.avg(x)
+        x = torch.nn.functional.adaptive_avg_pool2d(x, output_size=(1, 1))
         x = x.reshape(x.size(0), -1)
         x = self.fc(x)
         x = softmax(x, dim=1)
@@ -143,12 +145,12 @@ def main():
         transform=transforms.Compose([transforms.ToTensor()])
     )
 
-    EPOCHS = 1
+    EPOCHS = 10
     BATCH_SIZE = 64
     lr = 1e-4
 
-    train_loader = DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader = DataLoader(dataset=test_set, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=BATCH_SIZE, shuffle=True)
 
     model = ResNet(in_channels=1, n_classes=10)
     summary(model, (1, 28, 28))
@@ -174,6 +176,9 @@ def main():
 
             losses = AverageValueMeter()
             for x, y_idx in loader:
+                if losses.n > 10:
+                    break
+
                 # convert label to one-hot encoded
                 y = torch.zeros((x.size(0), 10))
                 y[torch.arange(x.size(0)), y_idx] = 1.0
@@ -184,8 +189,9 @@ def main():
 
                 # use custom implemented cross-entropy
                 # batch loss
-                loss = -torch.mean(y * torch.log(y_prim))
-                print(loss)
+                # loss = -torch.mean(y * torch.log(y_prim + 1e-8))
+                loss = -torch.mean(torch.log(y_prim + 1e-8)[torch.arange(BATCH_SIZE), y_idx])
+                # print(loss)
 
                 # loss.to('cpu').item() => single scalar value
                 # loss.to('cpu').data.numpy() => matrix
@@ -204,3 +210,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# TODO: add pytorch ModuleList, so all modules can see each other,
+#  instead of python list of Modules (also so we could query trainable parameters)
