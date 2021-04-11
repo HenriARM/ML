97c4c1b112d40cee92fbf297661054577713b756
HenriARM <henriarm@yandex.ru>
2021-04-12 02:53:35 +0300

    Fixed

--- tensorboard/run.sh
+++ tensorboard/run.sh
@@ -1,1 +1,4 @@
-python ./tensorboard-resnet.py -learning_rate 0.001 -batch_size 32 -epochs 10 
+python ./tensorboard-resnet.py -learning_rate 0.001 -batch_size 32 -epochs 5 &
+python ./tensorboard-resnet.py -learning_rate 0.01 -batch_size 64 -epochs 5
+wait
+# tensorboard --logdir=/Users/henrygabrielyan/Desktop/python/ML/tensorboard/seq-resnet
