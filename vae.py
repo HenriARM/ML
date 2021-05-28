4bde366f029eb7cec433ad71c95f86d76fafb5a2
HenriARM <henriarm@yandex.ru>
2021-05-28 15:49:05 +0300

    Fixed

--- vae.py
+++ vae.py
@@ -9,13 +9,14 @@ import torch.utils.data
 import scipy.ndimage
 
 VAE_BETA = 0.1
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 EPOCHS = 400
 LEARNING_RATE = 1e-3
 NOISINESS = 0.0
 
 run_path = ''
 
+# TODO: add arguments
 MAX_LEN = 200  # limit max number of samples otherwise too slow training (on GPU use all samples / for final training)
 DEVICE = 'cpu'
 if torch.cuda.is_available():
@@ -30,18 +31,14 @@ INCLUDE_CLASSES = []  # empty include all
 
 class DatasetCustom(torch.utils.data.Dataset):
     def __init__(self, is_train):
-
         data_tmp = torchvision.datasets.MNIST(
-            root='../data',
+            root='./datasets',
             train=is_train,
-            # split='byclass',
             download=True
         )
-
         global INCLUDE_CLASSES
         if len(INCLUDE_CLASSES) == 0:
             INCLUDE_CLASSES = np.arange(len(data_tmp.classes)).tolist()
-
         self.data = []
         for x, y_idx in data_tmp:
             if y_idx in INCLUDE_CLASSES:
@@ -62,25 +59,18 @@ class DatasetCustom(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         pil_y, label_idx = self.data[idx]
         np_y = np.array(pil_y).transpose()  # (28, 28)
-
         if NOISINESS > 0:
             noise = np.random.rand(*np_y.shape)
             np_x = np.where(noise < NOISINESS, 0, np_y)
         else:
             np_x = np.array(np_y)
-
         np_y = np.expand_dims(np_y, axis=0)  # (C, W, H)
         np_y = self.normalize(np_y)
-        y = torch.FloatTensor(np_y)
-
         np_x = np.expand_dims(np_x, axis=0)  # (C, W, H)
         np_x = self.normalize(np_x)
-        x = torch.FloatTensor(np_x)
-
         label = np.zeros((len(INCLUDE_CLASSES),))
         label[label_idx] = 1.0
-        label = torch.FloatTensor(label)
-        return x, y, label
+        return torch.FloatTensor(np_x), torch.FloatTensor(np_y), torch.FloatTensor(label)
 
 
 class VAE(torch.nn.Module):
@@ -169,56 +159,42 @@ def main():
     )
 
     model = VAE()
-    dummy = torch.randn((32, 1, 28, 28))
-    y = model.forward(dummy)
-
-    loss_func = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
     model = model.to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     metrics = {}
     for stage in ['train', 'test']:
-        for metric in [
-            'loss, loss_rec, loss_kl'
-        ]:
+        for metric in ['loss', 'loss_rec', 'loss_kl']:
             metrics[f'{stage}_{metric}'] = []
 
     for epoch in range(1, 100):
-
         for data_loader in [data_loader_train, data_loader_test]:
             metrics_epoch = {key: [] for key in metrics.keys()}
-
             stage = 'train'
+            model = model.train()
+            torch.set_grad_enabled(True)
             if data_loader == data_loader_test:
                 stage = 'test'
-
-            for x in data_loader:
-
+                model = model.eval()
+                torch.set_grad_enabled(False)
+            for x, y, _ in data_loader:
                 x = x.to(DEVICE)
                 y = y.to(DEVICE)
 
-                model = model.train()
-
                 y_prim, z, z_sigma, z_mu = model.forward(x)
-                loss = 0
-                # TODO loss function
-
                 loss_rec = torch.mean((y_prim - y) ** 2)
                 loss_kl = torch.mean(
                     VAE_BETA * torch.mean(-0.5 * (2 * torch.log(z_sigma + 1e-8) - z_sigma - z_mu ** 2 + 1), axis=0))
                 loss = loss_rec + loss_kl
 
-                # TODO: add to others
                 metrics_epoch[f'{stage}_loss'].append(loss.cpu().item())  # Tensor(0.1) => 0.1f
+                metrics_epoch[f'{stage}_loss_kl'].append(loss_kl.cpu().item())
+                metrics_epoch[f'{stage}_loss_rec'].append(loss_rec.cpu().item())
 
                 if data_loader == data_loader_train:
                     loss.backward()
                     optimizer.step()
                     optimizer.zero_grad()
-
-                np_y_prim = y_prim.cpu().data.numpy()
-
                 # TODO save model at best loss
 
             metrics_strs = []
@@ -239,19 +215,16 @@ def main():
             plts += plt.plot(value, f'C{c}', label=key)
             ax = plt.twinx()
             c += 1
-
         plt.legend(plts, [it.get_label() for it in plts])
-
         for i, j in enumerate([4, 5, 6, 16, 17, 18]):
             plt.subplot(4, 6, j)
             plt.imshow(x[i][0].T, cmap=plt.get_cmap('Greys'))
 
             plt.subplot(4, 6, j + 6)
-            plt.imshow(np_y_prim[i][0].T, cmap=plt.get_cmap('Greys'))
+            plt.imshow(y_prim.cpu().data.numpy()[i][0].T, cmap=plt.get_cmap('Greys'))
 
         plt.tight_layout(pad=0.5)
         plt.show()
-
 
 
 if __name__ == '__main__':
