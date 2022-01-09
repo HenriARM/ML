a0cf1ec8661ab7e659a627d9916b9a16d66e59ea
HenriARM <henriarm@yandex.ru>
2022-01-09 23:31:57 +0200

    Updated comments and added ReplayPriorityMemory

--- dqn.py
+++ dqn.py
@@ -38,6 +38,46 @@ if not torch.cuda.is_available():
     args.device = 'cpu'
 
 
+class ReplayPriorityMemory:
+    def __init__(self, size, batch_size, prob_alpha=1):
+        self.size = size
+        self.batch_size = batch_size
+        self.prob_alpha = prob_alpha
+        self.memory = []
+        self.priorities = np.zeros((size,), dtype=np.float32)
+        self.pos = 0
+
+    def push(self, transition):
+        new_priority = np.median(self.priorities) if self.memory else 1.0
+
+        self.memory.append(transition)
+        if len(self.memory) > self.size:
+            del self.memory[0]
+        pos = len(self.memory) - 1
+        self.priorities[pos] = new_priority
+
+    def sample(self):
+        probs = np.array(self.priorities)
+        if len(self.memory) < len(probs):
+            probs = probs[:len(self.memory)]
+
+        probs += 1e-8
+        probs = probs ** self.prob_alpha
+        probs /= probs.sum()
+
+        indices = np.random.choice(len(self.memory), batch_size, p=probs)
+        samples = [self.memory[idx] for idx in indices]
+        return samples, indices
+
+    def update_priorities(self, batch_indices, batch_priorities):
+        for idx, priority in zip(batch_indices, batch_priorities):
+            self.priorities[idx] = priority.item()
+
+    def __len__(self):
+        return len(self.memory)
+
+
+# send state -> return q value of each next action
 class Model(nn.Module):
     def __init__(self, state_size, action_size, hidden_size):
         super(Model, self).__init__()
@@ -122,7 +162,6 @@ class DQNAgent:
 
         idxes = torch.arange(args.batch_size).to(args.device)  # 0, 1, 2 .. batch_size
 
-        # TODO: understand
         q_t0_all = self.q_model.forward(s_t0)
         q_t0 = q_t0_all[idxes, a_t0]
 
@@ -214,6 +253,6 @@ env.close()
 plt.ioff()
 plt.show()
 
-# TODO: read what is terminal state of this specific game == when is_end becomes True ?
-# TODO: can we know we won or lost episode? because agent getting out of screen is also terminal state
-# TODO: what is reward function of this game? it is based on distance of st1 - st0 compared to win position?
+'''
+* Information about terminal states and reward formulas of Lunar Lender https://gym.openai.com/envs/LunarLander-v2/
+'''
