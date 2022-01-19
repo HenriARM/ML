d393f7afa2ee70fd8bf6371a9faf28a8248c7fd1
HenriARM <henriarm@yandex.ru>
2022-01-19 05:17:15 +0200

    Updated ple_dqn for using with GridSearch

--- reinforcement_learning/ple_dqn.py
+++ reinforcement_learning/ple_dqn.py
@@ -1,3 +1,4 @@
+import os
 import ple
 import argparse
 import numpy as np
@@ -29,7 +30,6 @@ parser.add_argument('-batch_size', default=128, type=int)
 parser.add_argument('-episodes', default=10000, type=int)
 parser.add_argument('-replay_buffer_size', default=5000, type=int)
 
-# parser.add_argument('-target_update', default=3000, type=int)
 parser.add_argument('-hidden_size', default=128, type=int)
 
 parser.add_argument('-gamma', default=0.7, type=float)
@@ -191,84 +191,97 @@ class DQNAgent:
         return loss.cpu().item()
 
 
-game = ple.games.flappybird.FlappyBird()
-# game = ple.games.snake.Snake(width=512, height=512)
-# game = ple.games.pong.Pong(width=512, height=512)
-p = ple.PLE(game, fps=30, display_screen=args.is_render)
-p.init()
+def run():
+    game = ple.games.flappybird.FlappyBird()
+    # game = ple.games.snake.Snake(width=512, height=512)
+    # game = ple.games.pong.Pong(width=512, height=512)
+    p = ple.PLE(game, fps=30, display_screen=args.is_render)
+    p.init()
 
-plt.figure()
+    plt.figure()
 
-all_scores = []
-all_losses = []
-all_t = []
+    all_scores = []
+    all_losses = []
+    all_t = []
 
-agent = DQNAgent(len(p.getGameState()), len(p.getActionSet()))
-is_end = p.game_over()
+    agent = DQNAgent(len(p.getGameState()), len(p.getActionSet()))
+    is_end = p.game_over()
 
-# best_terminal_time = 0
-for e in range(args.episodes):
-    p.reset_game()
-    s_t0 = np.asarray(list(p.getGameState().values()), dtype=np.float32)
-    reward_total = 0
-    episode_loss = []
-    for t in range(args.max_steps):
-        a_t0_idx = agent.act(s_t0)
-        a_t0 = p.getActionSet()[a_t0_idx]
-        r_t1 = p.act(a_t0)
-        is_end = p.game_over()
-        s_t1 = np.asarray(list(p.getGameState().values()), dtype=np.float32)
+    # best_terminal_time = 0
+    for e in range(args.episodes):
+        p.reset_game()
+        s_t0 = np.asarray(list(p.getGameState().values()), dtype=np.float32)
+        reward_total = 0
+        episode_loss = []
+        for t in range(args.max_steps):
+            a_t0_idx = agent.act(s_t0)
+            a_t0 = p.getActionSet()[a_t0_idx]
+            r_t1 = p.act(a_t0)
+            is_end = p.game_over()
+            s_t1 = np.asarray(list(p.getGameState().values()), dtype=np.float32)
 
-        reward_total += r_t1
-        if r_t1 != 0:
-            print(f'reward = {r_t1}')
+            reward_total += r_t1
+            if r_t1 != 0:
+                print(f'reward = {r_t1}')
 
-        if t == args.max_steps - 1:
-            r_t1 = -100
-            is_end = True
+            if t == args.max_steps - 1:
+                r_t1 = -100
+                is_end = True
 
-        agent.replay_memory.push(
-            (s_t0, a_t0_idx, r_t1, s_t1, is_end)
-        )
-        s_t0 = s_t1
+            agent.replay_memory.push(
+                (s_t0, a_t0_idx, r_t1, s_t1, is_end)
+            )
+            s_t0 = s_t1
 
-        if len(agent.replay_memory) > args.batch_size:
-            loss = agent.replay()
-            episode_loss.append(loss)
+            if len(agent.replay_memory) > args.batch_size:
+                loss = agent.replay()
+                episode_loss.append(loss)
 
-        if is_end:
-            all_scores.append(reward_total)
-            all_losses.append(np.mean(episode_loss))
-            # if t > best_terminal_time:
-            #     best_terminal_time = t
-            break
+            if is_end:
+                all_scores.append(reward_total)
+                all_losses.append(np.mean(episode_loss))
+                # if t > best_terminal_time:
+                #     best_terminal_time = t
+                break
 
-    all_t.append(t)
-    msg = f'episode: {e}/{args.episodes} ' \
-          f'loss: {all_losses[-1]} ' \
-          f'score: {reward_total} ' \
-          f't: {t} ' \
-          f'e: {agent.epsilon}'
-    logging.info(msg)
-    print(msg)
+        all_t.append(t)
+        msg = f'episode: {e}/{args.episodes} ' \
+              f'loss: {all_losses[-1]} ' \
+              f'score: {reward_total} ' \
+              f't: {t} ' \
+              f'e: {agent.epsilon}'
+        logging.info(msg)
+        print(msg)
 
-    if e % 100 == 0 and not args.is_inference:
-        # save logs, graphics and weights during training
-        plt.clf()
+        if e % 100 == 0 and not args.is_inference:
+            # save logs, graphics and weights during training
+            plt.clf()
 
-        plt.subplot(3, 1, 1)
-        plt.ylabel('Score')
-        plt.plot(all_scores)
+            plt.subplot(3, 1, 1)
+            plt.ylabel('Score')
+            plt.plot(all_scores)
 
-        plt.subplot(3, 1, 2)
-        plt.ylabel('Loss')
-        plt.plot(all_losses)
+            plt.subplot(3, 1, 2)
+            plt.ylabel('Loss')
+            plt.plot(all_losses)
 
-        plt.subplot(3, 1, 3)
-        plt.ylabel('Steps')
-        plt.plot(all_t)
+            plt.subplot(3, 1, 3)
+            plt.ylabel('Steps')
+            plt.plot(all_t)
 
-        plt.xlabel('Episode')
+            plt.xlabel('Episode')
 
-        plt.savefig(f'{args.run_path}/plt-{e}.png')
-        torch.save(agent.q_model.cpu().state_dict(), f'{args.run_path}/model-{e}.pt')
+            plt.savefig(f'{args.run_path}/plt-{e}.png')
+            torch.save(agent.q_model.cpu().state_dict(), f'{args.run_path}/model-{e}.pt')
+
+
+def main():
+    print(f'Hyperparams {args}')
+    print(f'module name: {__name__}')
+    print(f'parent process: {os.getppid()}')
+    print(f'process id: {os.getpid()}\n')
+    run()
+
+
+if __name__ == '__main__':
+    main()
